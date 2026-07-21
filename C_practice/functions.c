55da3575be15fdaf791e9e8af61e5fd1e970577c
prishaX <prishaverma2222@gmail.com>
2026-07-21 11:35:17 +0530

    Reccursions practiced few basic program day 1

--- C_practice/functions.c
+++ C_practice/functions.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-/*
-MAX OF 4 NUMBERS FUNCTION 
-*/
+
 int max_of_four(int a,int b,int c,int d){
    int max;
     if(a>b && a>c){
@@ -23,20 +21,62 @@ int max_of_four(int a,int b,int c,int d){
     return max;
     }
 
-int calcum(x,y,z)
-int x,y,z;{
+int calcum(int x,int y,int z){
     int sum;
     sum=x+y+z;
     return sum;
 }
 
+int power(int base,int exp){
+   if (exp==0){
+       return 1;
+   }
+    return base*power(base,exp-1);
+}
+
+int sum_of_digits(int a){
+ int q,sum=0;
+     q = a%10;
+     sum=sum+q;
+     a=a/10;
+    
+ return sum;
+}
+
+int factorial(int n){
+    if(n==0)
+        return 1;
+   return n * factorial(n-1);
+}
 
 int main() {
+
     int a, b, c, d;
+    printf("enter 4 digits to check max and calc sum of firs three- \n");
     scanf("%d %d %d %d", &a, &b, &c, &d);
     int ans = max_of_four(a, b, c, d);
     printf("the max of the four numbers is: %d \n", ans);
+    
     int sumofthree=calcum(a,b,c);
-    printf("the sum of the three numbers is: %d \n", sumofthree);
+    printf("the sum of the first three numbers is: %d \n", sumofthree);
+
+    int base,exp;
+    printf("enter base and exp-");
+    scanf("%d %d", &base, &exp);
+    int p = power(base,exp);
+    printf(" %d raised to the power %d is %d\n",base,exp,p);
+
+    int digit;
+    printf("enter a digit to calc sum of digits-");
+    scanf("%d", &digit);
+    int sumdigits= sum_of_digits(digit);
+    printf(" sum of digits of %d is %d\n",digit,sumdigits);
+
+    int factdigit;
+    printf("enter a digit to calc factorial-");
+    scanf("%d", &factdigit);
+    int fact= factorial(factdigit);
+    printf("factorial of %d is %d\n",factdigit,fact);
+
     return 0;
 }
