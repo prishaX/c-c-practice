c2f0b12ad453911007bc26ebac156e391b9476ff
prishaX <prishaverma2222@gmail.com>
2026-07-21 10:14:09 +0530

    more Functions practice in C

--- C_practice/functions.c
+++ C_practice/functions.c
@@ -23,11 +23,20 @@ int max_of_four(int a,int b,int c,int d){
     return max;
     }
 
+int calcum(x,y,z)
+int x,y,z;{
+    int sum;
+    sum=x+y+z;
+    return sum;
+}
+
+
 int main() {
     int a, b, c, d;
     scanf("%d %d %d %d", &a, &b, &c, &d);
     int ans = max_of_four(a, b, c, d);
     printf("the max of the four numbers is: %d \n", ans);
-  
+    int sumofthree=calcum(a,b,c);
+    printf("the sum of the three numbers is: %d \n", sumofthree);
     return 0;
 }
