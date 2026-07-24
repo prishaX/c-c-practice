90dd8bad23bfeb1e6b62af036cf9c56ee0c91c98
prishaX <prishaverma2222@gmail.com>
2026-07-24 18:47:44 +0530

    constructors and destructors studied

--- Cpp practice/constructor_destructor.cpp
+++ Cpp practice/constructor_destructor.cpp
@@ -18,8 +18,26 @@ public:
 
 //non parameterized constructor 
 Teacher (){
-    cout << "Hello im constructor ";  
+    cout << "Hello im non parameterized constructor\n";  
     dept = "cs"; //this line automatically sets dept cs for all the objects that will be created so we can remove dept from main
+}
+
+//parameterized constructor 
+Teacher (string name, string dept, string subject, double salary) {
+    cout << "Hello im parameterized  constructor \n";
+    this->name=name; //this pointer points to the variable of the class when the variable name of the function and class are same
+    this->dept=dept; 
+    this->subject=subject;
+    this->salary=salary;
+}
+
+//copy constructor
+Teacher(Teacher &orgObj){   //here object is passed by reference, so the changes made in this constructor will also change the original object
+    cout << "Hello im copy constructor \n";
+    this->name=orgObj.name; 
+    this->dept=orgObj.dept;
+    this->subject=orgObj.subject;
+    this->salary=orgObj.salary;
 }
 
 //member functions
@@ -44,7 +62,21 @@ int main (){
  t1.setsalary(25000); //i set salary from this function calling here, i cant use t1.salary as salary is private
  cout << t1.name << endl;
  cout << t1.dept << endl;
+ cout << t1.subject << endl;
  cout << t1.getsalary() << endl;
+
+ Teacher t2("stuti", "doctory", "gynac", 150000);
+ cout << t2.name << endl;
+ cout << t2.dept << endl;
+ cout << t2.subject << endl;
+ cout << t2.getsalary() << endl;
+
+ Teacher t3(t2);
+ cout << t3.name << endl;
+ cout << t3.dept << endl;
+ cout << t3.subject << endl;
+ cout << t3.getsalary() << endl;
+
 
 return 0;
 }
