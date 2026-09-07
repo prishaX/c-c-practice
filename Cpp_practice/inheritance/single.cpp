eeba096bfb61be17199ce867535d58c0cec097c0
prishaX <prishaverma2222@gmail.com>
2026-09-07 12:02:50 +0530

    memory allocation of parent child studied in inheritance

--- Cpp_practice/inheritance/single.cpp
+++ Cpp_practice/inheritance/single.cpp
@@ -29,12 +29,38 @@ public:
     }
 };
 
+class Parent {
+    protected:
+    int a;
+    public:
+    Parent(){cout<<"Parent constructor called"<<endl;}
+    void show(){
+        cout<<"Parent a: "<<a<<endl;
+    }
+    ~Parent(){cout<<"Parent destructor called"<<endl;}
+};
+
+class Child: public Parent {
+    public:
+    Child(){cout<<"Child constructor called"<<endl;}
+    void print(){
+        show(); //accessing the public member function of parent class
+        cout<<"Parent through child: "<<a<<endl;
+    }
+
+    ~Child(){cout<<"Child destructor called"<<endl;}
+};
+//first constructor of parent is called nd then child constructor 
+// destructor of child is called first nd then destructor of parent is called
+
 int main(){
 
     Dog dog("Buddy");
     dog.Eat();   // Inherited from Animal class
     dog.sleep(); // Inherited from Animal class
     dog.Bark();  // Specific to Dog class
+    Parent p;
+    Child c;
 
     return 0;
 }
